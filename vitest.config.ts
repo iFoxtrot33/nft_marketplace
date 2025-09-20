3b5aaa3c855c868e6c5e0b027ac520cfc88c9b3a
Iurii Furman <iuriifurman@MacBook-Pro-Iurii.local>
2025-09-20 12:11:04 +0200

    feat: set up test workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,26 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "**/index.ts",
+        "**/index.tsx",
+        "**/*.d.ts",
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/.next/**",
+        "**/coverage/**",
+        "**/page.tsx",
+        "**/layout.tsx",
+        "**/*.config*",
+        "**/types/**",
+        "**/styles/**",
+
+      ],
+      reporter: ['text', 'html', 'lcov'],
+      reportOnFailure: true,
+    },
   },
   resolve: {
     alias: {
