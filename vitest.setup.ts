63aa94ca6c70885361dd43301b10df983ee91fd2
Iurii Furman <iuriifurman@MacBook-Pro-Iurii.local>
2025-09-21 15:01:59 +0200

    fix: tests 3

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,9 @@
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom'
+
+global.requestAnimationFrame = (callback: FrameRequestCallback) => {
+  return setTimeout(callback, 16) // ~60fps
+}
+
+global.cancelAnimationFrame = (id: number) => {
+  clearTimeout(id)
+}
